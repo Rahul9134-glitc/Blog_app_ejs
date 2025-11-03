337c278808d20e50c84208341f2bfd55d5659d74
Rahul vishwakarma <vishwakarmarahulramesh@gmail.com>
2025-11-03 18:17:21 +0530

    error solving

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.json());
 
+app.set('trust proxy', 1);
+
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
